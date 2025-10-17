0dd50b72aa31220630def78131dd647127f41e76
Meghna <meghnaanil98@gmail.com>
2025-10-17 20:57:29 +0530

    tasks management errors fixed

--- src/routes/aiRoutes.ts
+++ src/routes/aiRoutes.ts
@@ -4,7 +4,7 @@ import { AIController } from '../controllers/aiController';
 const router = Router();
 const aiController = new AIController();
 
-router.get('/:projectId/summarize', (req, res) => aiController.summarizeProject(req, res));
+router.post('/:projectId/summarize', (req, res) => aiController.summarizeProject(req, res));
 router.post('/:projectId/ask', (req, res) => aiController.askQuestion(req, res));
 
 export default router;
